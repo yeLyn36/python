f3af6bc954e7f03f9301ead0b2ac0ba030e33a8f
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-09-09 10:29:26 +0900

    add datetimemodule

--- 패키지/datetimemodule.py
+++ 패키지/datetimemodule.py
@@ -13,9 +13,14 @@ print("today - day :", today - day)
 
 
 # 내가 태어난 날은 무슨 요일일까
-day = datetime(2002, 3, 6, 0, 0, 0)
-birth = day.weekday()
-print("내가 태어난 요일 :", birth)
+birthday = datetime(2002, 3, 6, 0, 0, 0)
+print("내가 태어난 요일 :", "월화수목금토일"[birthday.weekday()], "요일")
 
 # 나는 며칠 살았을까
+life = today - birthday
+print('내가 살아온 일 수 :', life.days)
+
 # 올해 크리스마스 며칠 남았을까
+christmas = datetime(2019, 12, 25, 0, 0, 0)
+left = christmas - today
+print("올해 크리스마스는 ", left.days, "일 남았습니다.")
