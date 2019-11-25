00ed4d3709206f23b7cc7ca8bd80d6e58175d288
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-11-25 09:47:32 +0900

    create html file

--- IX. 프로젝트/web_crawling.py
+++ IX. 프로젝트/web_crawling.py
@@ -5,10 +5,17 @@ if __name__ == "__main__":
     data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=695796")
     soup = BeautifulSoup(data, "lxml")  #httpResponse -> HTML
     print(soup)
+    html = "<html><head><meta charset='utf-8'></head><body>"
     cartoon_titles = soup.find_all("td", attrs= {"class":"title"}) # <td class="title"> --- </td>
     for cartoon_title in cartoon_titles:                        # cartoon_title[:2}
         title = cartoon_title.find("a").text                        #텍스트 가져오기 <a>text</a>
         link = cartoon_title.find("a").get("href")                  #태그의 속성값 가져오기 <a href="">text</a>
         link = "http://comic.naver.com" + link
-        print(title)
-        print(link)
+        # print(title)
+        # print(link)
+        html += "<a href='{}'>{}</a><br/>".format(link, title)
+
+    html += "</body></html>"
+    # print(html)
+    with open("내일.html", "w", encoding='utf8') as f:
+        f.write(html)
