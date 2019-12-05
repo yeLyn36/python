d2c8a0f933c5dbb65a1ffad94a4f887f7431b204
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-12-05 10:11:08 +0900

    pyautogui

---
+++ IX. 프로젝트/mouse.py
@@ -0,0 +1,12 @@
+import pyautogui as pag
+
+if __name__ == '__main__':
+    pag.moveTo(280, 1053)
+    # pag.click()
+    # pag.click()
+    # pag.click(2)
+    # pag.doubleClick()
+    # pag.move(100, 200, 1)
+    # pag.rightClick()
+    # pag.click(button='right')
+    pag.drag(0, 200, duration=1)
